a148d49b20f9b40d41d61fa77b1a91cbd4aa8680
H2YDEN <h2yden2@gmail.com>
2024-01-31 02:29:58 -0800

    fizzBuzz implementation working

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,16 @@
 // Write your solution below this line:
+for (let i = 1; i < 51; i++) {
+    switch(true) {
+        case ((i % 3 === 0) && (i % 5 === 0)):
+            console.log('fizzbuzz');
+            break;
+        case (i % 3 === 0):
+            console.log('fizz');
+            break;
+        case (i % 5 === 0):
+            console.log('buzz')
+            break;
+        default:
+            console.log(i);
+    }
+}
